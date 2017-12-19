037f41ab867a9b0554b1662028af6c3b87075dfe
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 19:22:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,18 @@
 #write your code here
-loop do
-  def countdown
-    x = 10
-    while x > 0
-      puts "#{x} SECOND(S)!"
-      x -= 1
-    end
-    return "HAPPY NEW YEAR!"
+
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
