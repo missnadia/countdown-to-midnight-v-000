ecdd026019c78f3d94c95fdd1fd4256e9f93d31f
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 19:19:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
-
-def countdown
+loop do
+  def countdown
+    x = 10
+    while x > 0
+      puts "#{x} SECOND(S)!"
+      x -= 1
+    end
+    return "HAPPY NEW YEAR!"
+  end
 end
